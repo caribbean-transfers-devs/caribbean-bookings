bc8194f7087dc2bb88fa419c497e71601f317af5
caribbean-transfers <development@caribbean-transfers.com>
2023-09-08 09:06:36 -0500

    [UPDATE] TPV

--- app/Http/Controllers/Tpv/TpvController.php
+++ app/Http/Controllers/Tpv/TpvController.php
@@ -2,18 +2,35 @@
 namespace App\Http\Controllers\Tpv;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TpvRequest;
 use App\Repositories\Tpv\TpvRepository;
 use App\Repositories\Tpv\AutocompleteRepository;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class TpvController extends Controller
-{
-    public function index(Request $request, TpvRepository $tpvRepository){
+{   
+    public function handler(Request $request, TpvRepository $tpvRepository){
+        return $tpvRepository->handler($request);
+    }
+
+    public function index(Request $request, $code, TpvRepository $tpvRepository){     
+
+        if (!$request->code) {
+            return response()->json([
+                    'error' => [
+                        'code' => 'required_params', 
+                        'message' =>  $validator->errors()->all() 
+                    ]
+                ], Response::HTTP_BAD_REQUEST);
+        }
+
         return $tpvRepository->index($request);
     }
 
-    public function detail(Request $request, AutocompleteRepository $autocompleteRepository)
+    /*public function detail(Request $request, AutocompleteRepository $autocompleteRepository)
     {
         return $autocompleteRepository->search($request);
-    }
+    }*/
 }
--- resources/views/tpv/index.blade.php
+++ resources/views/tpv/index.blade.php
@@ -2,11 +2,67 @@
 @section('title') TPV @endsection
 
 @push('up-stack')
+    <link href="{{ mix('/assets/css/tpv/index.min.css') }}" rel="preload" as="style" >
+    <link href="{{ mix('/assets/css/tpv/index.min.css') }}" rel="stylesheet" > 
 @endpush
 
 @push('bootom-stack')
 @endpush
 
 @section('content')
-123
+    <div class="container-fluid p-0">
+
+        <div class="mb-3">
+            <h1 class="h3 d-inline align-middle">TPV</h1>            
+        </div>
+
+        <div class="row">
+
+            <div class="col-xs-12">
+                <div class="bookingbox">
+                    <div class="type">
+                        <label class="form-label" for="serviceTypeForm">Tipo de viaje</label>
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                            <label class="form-check-label" for="flexSwitchCheckDefault">Viaje redondo</label>
+                        </div>
+                    </div>                            
+                    <div class="from">
+                        <label class="form-label" for="serviceTypeForm">Desde</label>
+                        <input type="text" class="form-control" id="serviceFromForm">
+                    </div>
+                    <div class="to">
+                        <label class="form-label" for="serviceTypeForm">Hacia</label>
+                        <input type="text" class="form-control" id="serviceFromForm">
+                    </div>
+                    <div class="language">
+                        <label class="form-label" for="serviceTypeForm">Idioma</label>
+                        <select class="form-control mb-2">
+                            <option value="USD">EN</option>
+                            <option value="MXN">ES</option>
+                        </select>
+                    </div>
+                    <div class="passengers">
+                        <label class="form-label" for="serviceTypeForm">Pasajeros</label>
+                        <select class="form-control mb-2">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                        </select>
+                    </div>
+                    <div class="rate_group">
+                        <label class="form-label" for="serviceTypeForm">Grupo de tarifa</label>
+                        <select class="form-control mb-2">
+                            <option value="xLjDl18">Default</option>                                    
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-4">
+            </div>
+            <div class="col-xl-8">
+            </div>
+
+        </div>
+    </div>
 @endsection
