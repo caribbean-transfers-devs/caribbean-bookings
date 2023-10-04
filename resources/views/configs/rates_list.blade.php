aa876a307e48e85790e70111e9366db178e1bdb4
caribbean-transfers <development@caribbean-transfers.com>
2023-10-04 10:46:26 -0500

    [UPDATE] Cambios en permisos

--- resources/views/configs/rates_list.blade.php
+++ resources/views/configs/rates_list.blade.php
@@ -4,7 +4,7 @@
 
 @if(sizeof($rates) >= 1)
     <form id="editPriceForm">
-        @if (!RoleTrait::hasPermission(34))
+        @if (RoleTrait::hasPermission(34))
             <button type="button" class="btn btn-sm btn-success btnUpdateRates">Actualizar Tarifas</button>
         @endif
         @foreach($rates as $key => $value)
@@ -32,7 +32,7 @@
                                 <input type="text" class="form-control" name="price[{{ $value->id }}][round_trip]" value="{{ $value->round_trip }}">
                             </div>
                         </div>
-                        @if (!RoleTrait::hasPermission(35))
+                        @if (RoleTrait::hasPermission(35))
                             <button class="btn btn-sm btn-danger" type="button" onclick="deleteItem({{ $value->id }})" data-id="{{ $value->id }}">Eliminar</button>
                         @endif
                     </div>
@@ -66,7 +66,7 @@
                                 <input type="text" class="form-control" name="price[{{ $value->id }}][up_8_rt]" value="{{ $value->up_8_rt }}">
                             </div>
                         </div>
-                        @if (!RoleTrait::hasPermission(35))
+                        @if (RoleTrait::hasPermission(35))
                             <button class="btn btn-sm btn-danger" type="button" onclick="deleteItem({{ $value->id }})" data-id="{{ $value->id }}">Eliminar</button>
                         @endif
                     </div>
@@ -74,7 +74,7 @@
                 
             </div>
         @endforeach
-        @if (!RoleTrait::hasPermission(34))
+        @if (RoleTrait::hasPermission(34))
             <button type="button" class="btn btn-sm btn-success btnUpdateRates">Actualizar Tarifas</button>
         @endif
     </form>
@@ -106,7 +106,7 @@
                         <input type="text" class="form-control" value="0.00" name="round_trip">
                     </div>
                 </div>
-                @if (!RoleTrait::hasPermission(33))
+                @if (RoleTrait::hasPermission(33))
                     <button class="btn btn-sm btn-success" type="button" id="btn_add_rate">Agregar Tarifa</button>
                 @endif
             </div>
@@ -140,7 +140,7 @@
                         <input type="text" class="form-control" value="0.00" name="up_8_rt">
                     </div>
                 </div>
-                @if (!RoleTrait::hasPermission(33))
+                @if (RoleTrait::hasPermission(33))
                     <button class="btn btn-sm btn-success" type="button" id="btn_add_rate">Agregar Tarifa</button>
                 @endif
             </div>
