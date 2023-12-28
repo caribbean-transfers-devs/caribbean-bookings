cefe253e217a5819bc16dd23523b587f4c18d3e9
caribbean-transfers <development@caribbean-transfers.com>
2023-12-28 16:45:38 -0500

    Update DashboardController.php

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\Dashboard;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repositories\Dashboards\DashboardRepository;
+use App\Traits\RoleTrait;
 
 class DashboardController extends Controller
 {   
+    use RoleTrait;
+    
     public function index(DashboardRepository $dashboard){
         return $dashboard->index();        
     }
@@ -14,7 +17,7 @@ class DashboardController extends Controller
         if(!RoleTrait::hasPermission(42)){
             abort(403, 'NO TIENE AUTORIZACIÓN.');
         }
-        
+
         return $dashboard->admin();        
     }
 }
