39b52d59c1215b0aa89999b2bf97b2f34819af6b
caribbean-transfers <development@caribbean-transfers.com>
2024-03-14 16:01:13 -0500

    Update master.blade.php

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -73,7 +73,7 @@
                         </li>
                     @endif
 
-                    @if(RoleTrait::hasPermission(43) || RoleTrait::hasPermission(44) || RoleTrait::hasPermission(45))
+                    @if(RoleTrait::hasPermission(43) || RoleTrait::hasPermission(44) || RoleTrait::hasPermission(45) || RoleTrait::hasPermission(50))
                         <li class="sidebar-item">
                             <a href="#reports" data-bs-toggle="collapse" class="sidebar-link collapsed">
                                 <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Reportes</span>
