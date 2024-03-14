58d152d5cb01d1bd8bf18468ff5ed5fbdfd9d0e0
caribbean-transfers <development@caribbean-transfers.com>
2024-03-14 13:37:00 -0500

    [UPDATE] Módulo de conciliación de pagos

---
+++ app/Http/Controllers/Reports/CashController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Controllers\Reports;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\Reports\CashRepository;
+use App\Traits\RoleTrait;
+
+class CashController extends Controller
+{
+    use RoleTrait;
+
+    public function index(Request $request, CashRepository $cashRepository){
+        if(!$this->hasPermission(50)){
+            abort(403, 'NO TIENE AUTORIZACIÓN.');
+        }
+        return $cashRepository->index($request);
+    }
+
+    public function update(Request $request, CashRepository $cashRepository){
+        if(!$this->hasPermission(50)){
+            abort(403, 'NO TIENE AUTORIZACIÓN.');
+        }
+        return $cashRepository->update($request);
+    }
+}
