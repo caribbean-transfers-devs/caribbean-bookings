3102698d01d64a5d0833c0d0279997cb7fcdc932
victor daniel dorantes chay <victordorantes_1991@hotmail.com>
2024-06-19 18:03:38 -0500

    update

--- resources/views/operation/operations.blade.php
+++ resources/views/operation/operations.blade.php
@@ -506,7 +506,7 @@
                 const __btn_open_modal_comment = ( __Row != null ? __Row.querySelector('td:nth-child(19)') : "" );
                 console.log(__btn_comment, __Row, __indicators, __btn_open_modal_comment);
                 __btn_comment.dataset.status = data.status;
-                __indicators.innerText = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square bs-popover" data-bs-container="body" data-bs-trigger="hover" data-bs-content="'+ data.value +'"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>';
+                __indicators.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square bs-popover" data-bs-container="body" data-bs-trigger="hover" data-bs-content="'+ data.value +'"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>';
             }
                         
             Snackbar.show({
