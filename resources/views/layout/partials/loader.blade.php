e2dff92d5206e07cfbc9d3507845d8c1bf118172
caribbean-transfers <development@caribbean-transfers.com>
2024-07-24 10:13:59 -0500

    [UPDATE] Se quita el loader

--- resources/views/layout/partials/loader.blade.php
+++ resources/views/layout/partials/loader.blade.php
@@ -1,4 +1,5 @@
-    <!-- BEGIN LOADER -->
+@if(false)
+<!-- BEGIN LOADER -->
     <div id="load_screen" class="d-none"> <div class="loader"> <div class="loader-content">
         {{-- <div class="spinner-grow align-self-center"></div> --}}
         <img src="/assets/img/logos/brand.svg" alt="loading" class="img-fluid" width="70">
@@ -6,3 +7,4 @@
         <p class="text-white mb-0" style="font-size: 16px;">Un momento por favor...</p>
     </div></div></div>
     <!--  END LOADER -->
+@endif
