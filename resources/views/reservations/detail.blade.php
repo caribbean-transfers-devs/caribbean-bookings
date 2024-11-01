e69eb793792e0c864dd304d939133b56c4705015
victor daniel darantes chay <victordorantes_1991@hotmail.com>
2024-11-01 15:23:38 -0600

    Fix flight operation and detail booking

--- resources/views/reservations/detail.blade.php
+++ resources/views/reservations/detail.blade.php
@@ -632,10 +632,14 @@
                                         <td class="text-start">{{ $payment->reference }}</td>
                                         <td class="text-center">
                                             @if (RoleTrait::hasPermission(15))
-                                            <a href="#" data-bs-toggle="modal" data-bs-target="#servicePaymentsModal" onclick="getPayment({{ $payment->id }})"><i class="align-middle" data-feather="edit-2"></i></a>
+                                                <a href="#" data-bs-toggle="modal" data-bs-target="#servicePaymentsModal" onclick="getPayment({{ $payment->id }})">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 align-middle"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
+                                                </a>
                                             @endif
                                             @if (RoleTrait::hasPermission(16))
-                                            <a href="#" onclick="deletePayment({{ $payment->id }})"><i class="align-middle" data-feather="trash"></i></a>
+                                                <a href="#" onclick="deletePayment({{ $payment->id }})">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash align-middle"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+                                                </a>
                                             @endif
                                         </td>
                                     </tr>
