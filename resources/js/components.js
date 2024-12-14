10fd1a141c81dcf73e7c1671c24ca8cf615a181c
victor daniel darantes chay <victordorantes_1991@hotmail.com>
2024-12-13 23:54:51 -0600

    [UPDATE] pagination dataTable reports

--- resources/js/components.js
+++ resources/js/components.js
@@ -69,7 +69,7 @@ let components = {
         _settings.oLanguage = {
             "sProcessing": "Procesando...",
             "sZeroRecords": "No se encontraron resultados",             
-            "sInfo": "Mostrando _TOTAL_ registros",
+            "sInfo": ( action == "fixedheaderPagination" ? "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros" : "Mostrando _TOTAL_ registros" ),
             "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
             "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
             "sSearchPlaceholder": components.getTranslation("table.search") + "...",
