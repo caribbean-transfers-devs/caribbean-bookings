97de0402e4da84c291c2cdb62a2fd1d611322ed6
victor daniel darantes chay <victordorantes_1991@hotmail.com>
2024-12-16 16:07:43 -0600

    [UPDATE] dashboard call center agent

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,25 +4,40 @@ namespace App\Http\Controllers\Dashboard;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repositories\Dashboards\DashboardRepository;
+use App\Repositories\Operation\OperationRepository;
 use App\Traits\RoleTrait;
 
 class DashboardController extends Controller
 {   
     use RoleTrait;
 
-    public function index(DashboardRepository $dashboard){
+    private $DashboardRepository;
+    private $ReportsRepository;    
+
+    public function __construct(DashboardRepository $DashboardRepository, OperationRepository $OperationRepository)
+    {
+        $this->DashboardRepository = $DashboardRepository;
+        $this->OperationRepository = $OperationRepository;
+    }    
+
+    public function index(Request $request){
         $roles = session()->get('roles');
         // if( ( !RoleTrait::hasPermission(42)) || ( !RoleTrait::hasPermission(62)) || ( !RoleTrait::hasPermission(63)) ){
         //     abort(403, 'NO TIENE AUTORIZACIÓN.');
         // }
-        return $dashboard->index($dashboard);   
+        
+        if( in_array(4, $roles['roles']) ){
+            return $this->OperationRepository->reservations($request);
+        }else{
+            return $this->DashboardRepository->index($request);
+        }
     }
 
-    public function admin(DashboardRepository $dashboard){
+    public function admin(Request $request){
         if(!RoleTrait::hasPermission(42)){
             //abort(403, 'NO TIENE AUTORIZACIÓN.');
         }
 
-        return $dashboard->admin();        
+        return $this->DashboardRepository->admin($request);        
     }
 }
